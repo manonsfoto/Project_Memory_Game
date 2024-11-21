e6c83ace3b720743e8caae814863b5da186d4722
Minyeong Jeong <megormin@gmail.com>
2024-11-21 11:34:50 +0100

    shuffle function added

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 import "./style.css";
+
+import { cardsArray } from "./arrays/cardsArray";
+import { ICard } from "./contracts/ICards";
+
+function shuffle(array: ICard[]) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
+// ===test
+const shuffledArray = shuffle(cardsArray);
+console.log(shuffledArray);

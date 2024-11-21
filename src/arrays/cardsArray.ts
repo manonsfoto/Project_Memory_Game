aa4b0a34e457121b87505a38b756c317287a871c
Minyeong Jeong <megormin@gmail.com>
2024-11-21 21:37:26 +0100

    tried to improve filpCardBack function

--- src/arrays/cardsArray.ts
+++ src/arrays/cardsArray.ts
@@ -1,75 +1,75 @@
 import { ICard } from "../contracts/ICards";
 
 export const cardsArray: ICard[] = [
-    {
-        id: 1,
-        emoji: "🍕",
-        isMatched: false,
-    },
+  {
+    id: 1,
+    emoji: "🧁",
+    isMatched: false,
+  },
 
-    {
-        id: 2,
-        emoji: "🍔",
-        isMatched: false,
-    },
+  {
+    id: 2,
+    emoji: "🍔",
+    isMatched: false,
+  },
 
-    {
-        id: 3,
-        emoji: "🍟",
-        isMatched: false,
-    },
+  {
+    id: 3,
+    emoji: "🍟",
+    isMatched: false,
+  },
 
-    {
-        id: 4,
-        emoji: "🌭",
-        isMatched: false,
-    },
+  {
+    id: 4,
+    emoji: "🌭",
+    isMatched: false,
+  },
 
-    {
-        id: 5,
-        emoji: "🍿",
-        isMatched: false,
-    },
+  {
+    id: 5,
+    emoji: "🍿",
+    isMatched: false,
+  },
 
-    {
-        id: 6,
-        emoji: "🥐",
-        isMatched: false,
-    },
+  {
+    id: 6,
+    emoji: "🥐",
+    isMatched: false,
+  },
 
-    {
-        id: 7,
-        emoji: "🥯",
-        isMatched: false,
-    },
+  {
+    id: 7,
+    emoji: "🍸",
+    isMatched: false,
+  },
 
-    {
-        id: 8,
-        emoji: "🥗",
-        isMatched: false,
-    },
+  {
+    id: 8,
+    emoji: "🥗",
+    isMatched: false,
+  },
 
-    {
-        id: 9,
-        emoji: "🥪",
-        isMatched: false,
-    },
+  {
+    id: 9,
+    emoji: "🥪",
+    isMatched: false,
+  },
 
-    {
-        id: 10,
-        emoji: "🌮",
-        isMatched: false,
-    },
+  {
+    id: 10,
+    emoji: "🌮",
+    isMatched: false,
+  },
 
-    {
-        id: 11,
-        emoji: "🍣",
-        isMatched: false,
-    },
+  {
+    id: 11,
+    emoji: "🍣",
+    isMatched: false,
+  },
 
-    {
-        id: 12,
-        emoji: "🍥",
-        isMatched: false,
-    },
+  {
+    id: 12,
+    emoji: "🍥",
+    isMatched: false,
+  },
 ];
--- src/main.ts
+++ src/main.ts
@@ -5,101 +5,144 @@ import { ICard } from "./contracts/ICards";
 
 const cardContainer = document.getElementById("container") as HTMLDivElement;
 const startBtn = document.querySelector("#game-btn") as HTMLButtonElement;
-const outputClicked = document.querySelector("#pairs-clicked") as HTMLSpanElement;
-const outputGuessed = document.querySelector("#pairs-guessed") as HTMLSpanElement;
+const outputClicked = document.querySelector(
+  "#pairs-clicked"
+) as HTMLSpanElement;
+const outputGuessed = document.querySelector(
+  "#pairs-guessed"
+) as HTMLSpanElement;
 
-let arrayForMatching: number[] = [];
+let arrayForMatching: ICard[] = [];
 let clickCounter: number = 0;
 let guessedCounter: number = 0;
-let isMatched: boolean = false;
-// Kartenanzeige
+// let isMatched: boolean = false;
 
 const renderCards = (shuffle1: ICard[], shuffle2: ICard[]) => {
-    const totalCards: ICard[] = [...shuffle1, ...shuffle2];
+  const totalCards: ICard[] = [...shuffle1, ...shuffle2];
 
-    totalCards.forEach((element: ICard) => {
-        const card = document.createElement("div");
-        card.classList.add("cards");
-        // card.textContent = element.emoji;
-        // ====test
-        const paragraph = document.createElement("p") as HTMLParagraphElement;
-        paragraph.textContent = element.emoji;
-        card.appendChild(paragraph);
-        paragraph.classList.add("unvisible");
-        // ====test
+  totalCards.forEach((element: ICard) => {
+    const card = document.createElement("div");
+    card.classList.add("cards");
 
-        cardContainer.appendChild(card);
+    const paragraph = document.createElement("p") as HTMLParagraphElement;
+    paragraph.textContent = element.emoji;
+    card.appendChild(paragraph);
+    paragraph.classList.add("unvisible");
+    cardContainer.appendChild(card);
 
-        card.addEventListener("click", () => {
-            card.classList.add("uncovered");
-            // test
-            paragraph.classList.remove("unvisible");
-            paragraph.classList.add("visible");
-            // test
-            arrayForMatching.push(element.id);
-            checkMatchingCards();
-            countClick();
-            countGuessed();
-        });
+    card.addEventListener("click", () => {
+      card.classList.add("uncovered", element.emoji);
+      paragraph.classList.remove("unvisible");
+      paragraph.classList.add("visible");
+
+      arrayForMatching.push(element);
+      checkMatchingCards();
+      countClick();
+      // countGuessed();
+      winGame();
     });
+  });
 };
 
 renderCards(shuffle(cardsArray), shuffle(cardsArray));
 
 function shuffle(array: ICard[]) {
-    return array.sort(() => Math.random() - 0.5);
+  return array.sort(() => Math.random() - 0.5);
 }
 
 // Flipback
-function flipCardsBack() {
-    const uncoveredCards = document.querySelectorAll(".uncovered");
-    uncoveredCards.forEach((card) => {
-        card.classList.remove("uncovered");
-        const paragraph = card.querySelector("p");
-        if (paragraph) {
-            paragraph.classList.remove("visible");
-            paragraph.classList.add("unvisible");
-        }
-    });
+
+function flipCardsBack(card1: HTMLDivElement, card2: HTMLDivElement) {
+  [card1, card2].forEach((card) => {
+    card?.classList.remove("uncovered");
+
+    const paragraph = card?.querySelector("p");
+
+    paragraph?.classList.remove("visible");
+    paragraph?.classList.add("unvisible");
+  });
+
+  // const uncoveredCards = document.querySelectorAll(".uncovered");
+  // uncoveredCards.forEach((card) => {
+  //   card.classList.remove("uncovered");
+  //   const paragraph = card.querySelector("p");
+  //   if (paragraph) {
+  //     paragraph.classList.remove("visible");
+  //     paragraph.classList.add("unvisible");
+  //   }
+  // });
 }
 
-function checkMatchingCards() {
-    console.log(arrayForMatching);
+function checkMatchingCards(): boolean {
+  if (arrayForMatching.length === 2) {
+    if (arrayForMatching[0].id === arrayForMatching[1].id) {
+      console.log("Hurray! these two Cards are matched!");
+      const matchedCard1 = document.querySelector(
+        `.${arrayForMatching[0].emoji}`
+      ) as HTMLDivElement;
+      const matchedCard2 = document.querySelector(
+        `.${arrayForMatching[1].emoji}`
+      ) as HTMLDivElement;
 
-    if (arrayForMatching.length === 2) {
-        if (arrayForMatching[0] === arrayForMatching[1]) {
-            console.log("Hurray! these two Cards are matched!");
-            guessedCounter++;
+      matchedCard1.classList.add("matched");
+      matchedCard2.classList.add("matched");
 
-            arrayForMatching = [];
-            return (isMatched = true);
-        } else {
-            console.log("Sorry, these two Cards are NOT matched");
-            setTimeout(() => {
-                flipCardsBack();
-                arrayForMatching = [];
-            }, 1000);
-            return (isMatched = false);
-        }
+      console.log("matched", matchedCard1, matchedCard2);
+
+      guessedCounter++;
+      arrayForMatching = [];
+
+      return true;
+    } else if (arrayForMatching[0].id !== arrayForMatching[1].id) {
+      console.log("Sorry, these two Cards are NOT matched");
+
+      const unmatchedCard1 = document.querySelector(
+        `.${arrayForMatching[0].emoji}`
+      ) as HTMLDivElement;
+      const unmatchedCard2 = document.querySelector(
+        `.${arrayForMatching[1].emoji}`
+      ) as HTMLDivElement;
+
+      if (unmatchedCard1 && unmatchedCard2) {
+        setTimeout(() => {
+          console.log("unmatched", unmatchedCard1, unmatchedCard2);
+
+          flipCardsBack(unmatchedCard1, unmatchedCard2);
+        }, 1000);
+      }
+      arrayForMatching = [];
+
+      return false;
     }
+  }
+  return false;
 }
 
 startBtn?.addEventListener("click", () => {
-    cardContainer.innerHTML = "";
-    clickCounter = 0;
-    guessedCounter = 0;
-    outputClicked.textContent = "";
-    outputGuessed.textContent = "";
+  cardContainer.innerHTML = "";
+  clickCounter = 0;
+  guessedCounter = 0;
+  outputClicked.textContent = "";
+  outputGuessed.textContent = "";
 
-    renderCards(shuffle(cardsArray), shuffle(cardsArray));
+  renderCards(shuffle(cardsArray), shuffle(cardsArray));
 });
 
 function countClick() {
-    clickCounter++;
-    console.log(clickCounter);
-    outputClicked.textContent = clickCounter.toString();
+  clickCounter++;
+  console.log(clickCounter);
+  outputClicked.textContent = Math.floor(clickCounter / 2).toString();
 }
 
-function countGuessed() {
-    outputGuessed.textContent = guessedCounter.toString();
+// function countGuessed() {
+//   outputGuessed.textContent = guessedCounter.toString();
+// }
+
+function winGame() {
+  const allMatchedCards = document.querySelectorAll(".matched");
+  console.log("allmatchedcards length:", allMatchedCards.length);
+  outputGuessed.textContent = allMatchedCards.length.toString();
+  if (allMatchedCards?.length === 12) {
+    console.log("Congrats!! You win!");
+  }
 }

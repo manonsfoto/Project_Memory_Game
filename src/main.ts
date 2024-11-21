93ee02d2b71d7dca21ca989db7ddc0775da1021f
Minyeong Jeong <megormin@gmail.com>
2024-11-21 13:05:43 +0100

    nothing to commit

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,7 @@
-import { cardsArray } from "./arrays/cardsArray";
-import { ICard } from "./contracts/ICards";
 import "./style.css";
 
-const cardContainer = document.getElementById("container") as HTMLDivElement;
-
-// Kartenanzeige
-
-const renderCards = (shuffle1: ICard[], shuffle2: ICard[]) => {
-  const totalCards: ICard[] = [...shuffle1, ...shuffle2];
-
-  totalCards.forEach((element: ICard) => {
-    console.log(element);
-
-    const card = document.createElement("div");
-    card.classList.add("cards");
-
-    cardContainer.appendChild(card);
-
-    return cardContainer;
-  });
-};
-
-renderCards(cardsArray, cardsArray);
+import { cardsArray } from "./arrays/cardsArray";
+import { ICard } from "./contracts/ICards";
 
 function shuffle(array: ICard[]) {
   return array.sort(() => Math.random() - 0.5);

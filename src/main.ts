c2f95a58fcdcadecd2399572e2ee56938d816358
griez97 <bryniarski.daniel@gmail.com>
2024-11-21 13:02:17 +0100

    flip cards

--- src/main.ts
+++ src/main.ts
@@ -7,24 +7,29 @@ const cardContainer = document.getElementById("container") as HTMLDivElement;
 // Kartenanzeige
 
 const renderCards = (shuffle1: ICard[], shuffle2: ICard[]) => {
-  const totalCards: ICard[] = [...shuffle1, ...shuffle2];
+    const totalCards: ICard[] = [...shuffle1, ...shuffle2];
 
-  totalCards.forEach((element: ICard) => {
-    console.log(element);
+    totalCards.forEach((element: ICard) => {
+        console.log(element);
 
-    const card = document.createElement("div");
-    card.classList.add("cards");
+        const card = document.createElement("div");
+        card.classList.add("cards");
+        card.textContent = element.emoji;
 
-    cardContainer.appendChild(card);
+        cardContainer.appendChild(card);
 
-    return cardContainer;
-  });
+        card.addEventListener("click", () => {
+            card.classList.add("uncovered");
+        });
+
+        return cardContainer;
+    });
 };
 
-renderCards(cardsArray, cardsArray);
+renderCards(shuffle(cardsArray), shuffle(cardsArray));
 
 function shuffle(array: ICard[]) {
-  return array.sort(() => Math.random() - 0.5);
+    return array.sort(() => Math.random() - 0.5);
 }
 
 // ===test
